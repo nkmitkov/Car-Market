63f6e498dbcc937b9793b8f7b034abbec084b350
Nick <nkmitkov@gmail.com>
2024-09-01 15:00:35 +0300

    Config Handlebars

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const configExpress = require("./config/configExpress");
+const configHandlebars = require("./config/configHandlebars");
 
 const app = express();
 const port = 3000;
 
 configExpress(app);
+configHandlebars(app);
