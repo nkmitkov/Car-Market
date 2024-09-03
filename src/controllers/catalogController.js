0a50340a31b69a04edeceb3eb451d74f7050df3e
Nick <nkmitkov@gmail.com>
2024-09-03 17:17:33 +0300

    Add details page with dynamic data

--- src/controllers/catalogController.js
+++ src/controllers/catalogController.js
@@ -12,11 +12,35 @@ router.get("/women", async (req, res) => {
     }
 });
 
+router.get("/women/:productId", async (req, res) => {
+    const productId = req.params.productId;
+
+    try {
+        const product = await catalogService.getById(productId).lean();
+
+        res.render("home/details", { ...product });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 router.get("/men", async (req, res) => {
     try {
         const products = await catalogService.getAllMens().lean();
 
         res.render("home/men", { products });
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+router.get("/men/:productId", async (req, res) => {
+    const productId = req.params.productId;
+    
+    try {
+        const product = await catalogService.getById(productId).lean();
+
+        res.render("home/details", { ...product });
     } catch (error) {
         console.log(error);
     }
--- src/services/catalogService.js
+++ src/services/catalogService.js
@@ -2,3 +2,4 @@ const Product = require("../models/Product");
 
 exports.getAllMens = () => Product.find({ sex: "men" });
 exports.getAllWomens = () => Product.find({ sex: "women" });
+exports.getById = (productId) => Product.findById(productId);
