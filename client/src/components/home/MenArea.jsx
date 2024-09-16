9ba562977e3f95b423b6fc914a923fa13d01903c
Nick <nkmitkov@gmail.com>
2024-09-16 13:25:59 +0300

    Show dynamic content on home page - men area

--- client/src/components/home/MenArea.jsx
+++ client/src/components/home/MenArea.jsx
@@ -1,4 +1,8 @@
+import { useFetch } from "../../hooks/useFetch";
+import HomeListItem from "../partials/HomeListItem";
+
 export default function MenArea() {
+    const { data: products, updateData } = useFetch("http://127.0.0.1:3000/catalog/men");
 
     return (
         <div style={{ margin: "50px auto 50px 200px" }}>
@@ -12,86 +16,9 @@ export default function MenArea() {
                         <div className="col-lg-6">
                             <div className="right-content">
                                 <div className="row">
-                                    <div className="col-lg-6">
-                                        <div className="right-first-image">
-                                            <div className="thumb">
-                                                <div className="inner-content">
-                                                    <h4>productName</h4>
-                                                    <span>$ price</span>
-                                                </div>
-                                                <div className="hover-content">
-                                                    <div className="inner">
-                                                        <h4>productName</h4>
-                                                        <p>description</p>
-                                                        <div className="main-border-button">
-                                                            <a href="#">Details</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <img src="/images/baner-right-image-01.jpg" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <div className="right-first-image">
-                                            <div className="thumb">
-                                                <div className="inner-content">
-                                                    <h4>Men</h4>
-                                                    <span>Best Clothes For Men</span>
-                                                </div>
-                                                <div className="hover-content">
-                                                    <div className="inner">
-                                                        <h4>Men</h4>
-                                                        <p>Lorem ipsum dolor sit amet, conservisii ctetur adipiscing elit incid.</p>
-                                                        <div className="main-border-button">
-                                                            <a href="#">Details</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <img src="/images/baner-right-image-02.jpg" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <div className="right-first-image">
-                                            <div className="thumb">
-                                                <div className="inner-content">
-                                                    <h4>Kids</h4>
-                                                    <span>Best Clothes For Kids</span>
-                                                </div>
-                                                <div className="hover-content">
-                                                    <div className="inner">
-                                                        <h4>Kids</h4>
-                                                        <p>Lorem ipsum dolor sit amet, conservisii ctetur adipiscing elit incid.</p>
-                                                        <div className="main-border-button">
-                                                            <a href="#">Details</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <img src="/images/baner-right-image-03.jpg" />
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6">
-                                        <div className="right-first-image">
-                                            <div className="thumb">
-                                                <div className="inner-content">
-                                                    <h4>Accessories</h4>
-                                                    <span>Best Trend Accessories</span>
-                                                </div>
-                                                <div className="hover-content">
-                                                    <div className="inner">
-                                                        <h4>Accessories</h4>
-                                                        <p>Lorem ipsum dolor sit amet, conservisii ctetur adipiscing elit incid.</p>
-                                                        <div className="main-border-button">
-                                                            <a href="#">Details</a>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <img src="/images/baner-right-image-04.jpg" />
-                                            </div>
-                                        </div>
-                                    </div>
+
+                                    {products.map(product => (<HomeListItem key={product._id} {...product} />))}
+
                                 </div>
                             </div>
                         </div>
---
+++ client/src/components/partials/HomeListItem.jsx
@@ -0,0 +1,33 @@
+export default function HomeListItem({
+    description,
+    id,
+    image,
+    price,
+    sex,
+    title,
+    _id,
+}) {
+
+    return (
+        <div className="col-lg-6">
+            <div className="right-first-image">
+                <div className="thumb">
+                    <div className="inner-content">
+                        <h4>{title}</h4>
+                        <span>$ {price}</span>
+                    </div>
+                    <div className="hover-content">
+                        <div className="inner">
+                            <h4>{title}</h4>
+                            <p>{description}</p>
+                            <div className="main-border-button">
+                                <a href={`/catalog/men/${_id}`}>Details</a>
+                            </div>
+                        </div>
+                    </div>
+                    <img src={image} alt={title} />
+                </div>
+            </div>
+        </div>
+    );
+};
